94d63a963df1b32065d3e25fec82747b78bb6257
blackford761 <covenantjenmartin69@gmail.com>
2026-07-12 17:43:36 +0700

    My first step using f-strings to change the output base on the input finished

---
+++ main.py
@@ -0,0 +1,4 @@
+print('world cup predictor')
+team_1 = input("Which team do you predict will win: ")
+team_2 = input("Which team do you predict will lose: ")
+print(f"{team_1} will win against {team_2}, vamos!")
