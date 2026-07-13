1b1254acfb935a1bb1ef81cf473c61ae6244a66b
blackford761 <covenantjenmartin69@gmail.com>
2026-07-13 22:28:58 +0700

    add def / function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print('world cup predictor')
-team_1 = input("Which team do you predict will win: ")
-team_2 = input("Which team do you predict will lose: ")
-print(f"{team_1} will win against {team_2}, vamos!")
+def world_cup_predictor():
+    print('world cup predictor')
+    team_1 = input("Which team do you predict will win: ")
+    team_2 = input("Which team do you predict will lose: ")
+    print(f"{team_1} will win against {team_2}, vamos!")
+
+world_cup_predictor()
