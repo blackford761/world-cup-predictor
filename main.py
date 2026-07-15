7f36f765faefccf8e48e1e5d849d4c127d9ee760
blackford761 <covenantjenmartin69@gmail.com>
2026-07-15 18:59:42 +0700

    Fidgeting my function with arguments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def world_cup_predictor():
+def world_cup_predictor(team_1 = "Argentina", team_2 = "England"):
     print('world cup predictor')
-    team_1 = input("Which team do you predict will win: ")
-    team_2 = input("Which team do you predict will lose: ")
-    print(f"{team_1} will win against {team_2}, vamos!")
+    print(team_1, "will win against", team_2, "vamos!")
 
-world_cup_predictor()
+team_1 = input("Which team do you predict will win: ")
+team_2 = input("Which team do you predict will lose: ") 
+world_cup_predictor(team_1, team_2)
