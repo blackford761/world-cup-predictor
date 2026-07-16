8d975aef023a8883406df17066e235dae85eff26
blackford761 <covenantjenmartin69@gmail.com>
2026-07-16 20:09:55 +0700

    trying to understand def command

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def world_cup_predictor(team_1 = "Argentina", team_2 = "England"):
-    print('world cup predictor')
-    print(team_1, "will win against", team_2, "vamos!")
+def world_cup_predictor(team_1, team_2):
 
-team_1 = input("Which team do you predict will win: ")
-team_2 = input("Which team do you predict will lose: ") 
-world_cup_predictor(team_1, team_2)
+    return (team_1 + " will win against " + team_2)
+
+print(world_cup_predictor("Argentina", "England"))       
+print(world_cup_predictor("Spain", "France"))       
+print(world_cup_predictor("Norway", "Brazil"))       
+    
